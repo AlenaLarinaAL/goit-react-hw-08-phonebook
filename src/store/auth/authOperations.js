a7de64599aed488ee4aed908b0542f9b81ae0e68
AlenaLarinaAL <alenalarina605@gmail.com>
2023-09-06 17:30:57 +0000

    add operations

---
+++ src/store/auth/authOperations.js
@@ -0,0 +1,46 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
+
+const setAuthHeader = token => {
+  axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+};
+const removeAuthHeader = () => {
+  axios.defaults.headers.common.Authorization = '';
+};
+
+export const register = createAsyncThunk(
+  'auth/register',
+  async (credentials, thunkApi) => {
+    try {
+      const { data } = await axios.post('/users/signup', credentials);
+      setAuthHeader(data.token);
+      return data;
+    } catch (error) {
+      return thunkApi.rejectWithValue(error.massege);
+    }
+  }
+);
+
+export const logIn = createAsyncThunk(
+  'auth/login',
+  async (credentials, thunkApi) => {
+    try {
+      const { data } = await axios.post('/users/login', credentials);
+      setAuthHeader(data.token);
+      return data;
+    } catch (error) {
+      return thunkApi.rejectWithValue(error.massege);
+    }
+  }
+);
+
+export const logOut = createAsyncThunk('auth/logout', async (_, thunkApi) => {
+  try {
+    await axios.post('/users/logout');
+    removeAuthHeader();
+  } catch (error) {
+    return thunkApi.rejectWithValue(error.massege);
+  }
+});
