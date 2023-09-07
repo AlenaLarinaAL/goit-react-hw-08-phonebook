d17afd27345eb0d16e3be312751ee483fdd394df
AlenaLarinaAL <alenalarina605@gmail.com>
2023-09-07 17:44:45 +0000

    add

--- src/index.js
+++ src/index.js
@@ -5,19 +5,19 @@ import { GlobalStyles, theme } from 'styles';
 import { Global, ThemeProvider } from '@emotion/react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { store } from './store/store';
-// import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from './store/store';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <ThemeProvider theme={theme}>
         <Global styles={GlobalStyles} />
-        {/* <PersistGate loading={null} persistor={persistor}> */}
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-        {/* </PersistGate> */}
+        <PersistGate loading={null} persistor={persistor}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </PersistGate>
       </ThemeProvider>
     </Provider>
   </React.StrictMode>
--- src/store/store.js
+++ src/store/store.js
@@ -1,15 +1,38 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { filter } from './filter/filterSlice';
 import { contactsApi } from './contacts/contactsSlice';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
+};
 
 export const store = configureStore({
   reducer: {
+    auth: persistReducer(authPersistConfig),
     filter: filter,
     [contactsApi.reducerPath]: contactsApi.reducer,
   },
 
-  middleware: getDefaultMiddleware => [
-    ...getDefaultMiddleware(),
-    contactsApi.middleware,
-  ],
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+  devTools: process.env.NODE_ENV === 'development',
 });
+
+export const persistor = persistStore(store);
